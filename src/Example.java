41354ef98dd3ef1526c623399ade572341490189
vlj2007 <vlj2007@yandex.ru>
2023-07-10 03:48:12 +0700

    I'm_add_maths_method

--- src/Example.java
+++ src/Example.java
@@ -2,7 +2,7 @@ public class Example {
     public static void main (String[] args) {
         int num;
         num = 100;
-        System.out.print("Значение num: " + num);
+        System.out.print("Значение num * 2: ");
         System.out.println(num);
     }
 }
