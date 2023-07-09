13f7e8e54cce91698f0d7b64b1878e3f352d2c65
vlj2007 <vlj2007@yandex.ru>
2023-07-10 03:12:28 +0700

    I'm_add_maths_string

---
+++ src/Example.java
@@ -0,0 +1,8 @@
+public class Example {
+    public static void main (String[] args) {
+        int num;
+        num = 100;
+        System.out.println("Значение num: " + num);
+        System.out.println(num);
+    }
+}
